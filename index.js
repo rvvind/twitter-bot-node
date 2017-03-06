442f5d9ad5961c0841652f8c27402501ca51644c
Arvind <rvvind@Gitanjalis-Mac-mini.local>
2017-03-06 00:38:12 -0800

    Polished retweet function and added lambda friendly functions

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
+var Twitter = require('twitter');
 var credentials = require('./credentials');
 var client = new Twitter(credentials);
 
 var query = "from:awscloud min_faves:50";
 
-client.get('search/tweets', {q: "node.js"}, function(err, tweets, response) { 
-	 if (err || !tweet.statuses) { 
-		console.log(err); 
-	} else { 
-		tweets.statuses.forEach(function(tweet) { 
-			console.log(tweet.user.screen_name + " " + tweet.text); 
-		}); 
-	} 
-});
+function searchAndTweet(succeed, fail) {
+        console.log("search and tweet");
+        client.get('search/tweets', {q: query, count: 15}, function(err, tweets, response) {
+                if (!tweets.statuses) {
+                        console.log(err);
+                }
+		tweets.statuses.forEach(function(tweet) {
+ 			var tweetId = tweet.id_str;
+			client.post('statuses/retweet/' + tweetId, function(err, tweet, id) {
+				// Will return an error if we try to retweet a tweet that we've already
+          			// retweeted.
+          			console.log(err || tweet.text);
+        		});
+    		});
+    		succeed("success");
+	});
+}
+
+exports.handler = function(event,context) {
+  searchAndTweet(context.succeed, context.fail);
+};
