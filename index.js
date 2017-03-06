057cd74aea0eeeb12a4c6632d8941978a032666c
Arvind <rvvind@Gitanjalis-Mac-mini.local>
2017-03-05 17:10:29 -0800

    first node.js callback function

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 var credentials = require('./credentials');
 var client = new Twitter(credentials);
+
+var query = "from:awscloud min_faves:50";
+
+client.get('search/tweets', {q: "node.js"}, function(err, tweets, response) { 
+	 if (err || !tweet.statuses) { 
+		console.log(err); 
+	} else { 
+		tweets.statuses.forEach(function(tweet) { 
+			console.log(tweet.user.screen_name + " " + tweet.text); 
+		}); 
+	} 
+});
