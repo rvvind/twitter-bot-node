8b66037fa07487614ae0236770dd6b95f840c422
Arvind Suryakumar <rvvind@Arvinds-MacBook-Pro.local>
2017-03-11 18:44:20 -0800

    Adding support for multiple queries

--- index.js
+++ index.js
@@ -2,24 +2,26 @@ var Twitter = require('twitter');
 var credentials = require('./credentials');
 var client = new Twitter(credentials);
 
-var query = "from:awscloud min_faves:50";
+var query = ["from:awscloud min_faves:50", "from:JeffBezos min_faves:500"];
 
 function searchAndTweet(succeed, fail) {
-        console.log("search and tweet");
-        client.get('search/tweets', {q: query, count: 15}, function(err, tweets, response) {
-                if (!tweets.statuses) {
-                        console.log(err);
-                }
-		tweets.statuses.forEach(function(tweet) {
- 			var tweetId = tweet.id_str;
-			client.post('statuses/retweet/' + tweetId, function(err, tweet, id) {
-				// Will return an error if we try to retweet a tweet that we've already
-          			// retweeted.
-          			console.log(err || tweet.text);
-        		});
-    		});
-    		succeed("success");
-	});
+  for (i=0; i < query.length; i++) {
+    console.log("search and tweet " + i);
+    client.get('search/tweets', {q: query[i], count: 15}, function(err, tweets, response) {
+      if (!tweets.statuses) {
+        console.log(err);
+      }
+      tweets.statuses.forEach(function(tweet) {
+        var tweetId = tweet.id_str;
+        client.post('statuses/retweet/' + tweetId, function(err, tweet, id) {
+        // Will return an error if we try to retweet a tweet that we've already
+                // retweeted.
+                console.log(err || tweet.text);
+        });
+      });
+      succeed("success");
+    });
+  }     
 }
 
 exports.handler = function(event,context) {
